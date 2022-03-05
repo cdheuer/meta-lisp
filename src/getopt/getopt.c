ac73c881ee4d6a44564589104132144e1431c6fd
Chris Heuer <chris@cdheuer.com>
2022-03-05 11:02:16 -0600

    getopt hello world

--- src/getopt/getopt.c
+++ src/getopt/getopt.c
@@ -1,1 +1,10 @@
 #include "getopt.h"
+
+int optopt;
+int opterr;
+int optreset;
+
+int getopt_long(int argc, char **argv, char *optstring, struct option *longopts, int *longindex)
+{
+	return 0;
+}
--- src/getopt/getopt.h
+++ src/getopt/getopt.h
@@ -1,5 +1,26 @@
 #ifndef GETOPT_H
 #define GETOPT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct option {
+	char *name;
+	int has_arg;
+	int *flag;
+	int val;
+};
+
+int getopt_long(int argc, char **argv, char *optstring, struct option *longopts, int *longindex);
+
+extern int optopt;
+extern int opterr;
+extern int optreset;
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
